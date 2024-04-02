df2785cc12c5ce6e0be8f637507a6005e0da2f13
Manasi sanjay Jadhav <101620713+manasisj2001@users.noreply.github.com>
2024-04-02 18:10:49 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+let username =document.getElementById("un") ;
+let password1=document.getElementById("pwd1") ;
+let password2= document.getElementById("pwd2") ;
+let phone = document.getElementById("phone") ;
+let email = document.getElementById("eml") ;
+
+function registration()
+{
+    let un = username.value ;
+    let pwd1 =password1.value ;
+    let pwd2 = password2.value ;
+    let phn = phone.value ;
+    let eml = email.value ;
+
+    
+    if(((un && phn && eml) != "") && (pwd1 === pwd2) )
+    {
+        let userData =new regData(un , pwd1 ,phn ,eml) ;
+        localStorage.setItem("userdetails" ,JSON.stringify(userData)) ;
+        alert("REGISTRATION SUCCESSFULLY") ;
+        open("login.html" ,"_self") ;
+    }
+    else{
+        alert("PASSWORD DOES NOT MATCH") ;
+    }
+
+}
+
+function regData(un,pass,phn,eml)
+{
+    this.un=un ;
+    this.pass =pass ;
+    this.phn =phn ;
+    this.eml=eml ;
+
+}
